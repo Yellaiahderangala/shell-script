a1d64ae0638402c644a8f2279420d0b417424834
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-11 23:38:41 +0530

    pushing third script

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("apple" "banana" "pomegranate")
+
+echo "first value : ${FRUITS[0]}"
+echo "first value : ${FRUITS[1]}"
+echo "first value : ${FRUITS[2]}"
+
+echo "all fruits: ${FRUITS[@]}"
