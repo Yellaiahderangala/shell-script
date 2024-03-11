6cfff2179ab717d46120fca7a8257d377345343d
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-11 23:16:42 +0530

    pushing third script

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,9 +2,9 @@
 
 echo "please enter your username:"
 read -s USERNAME #the value entered will be automatically atteched to USERNAME variable 
-echon "username is $USERNAME" # i am printing the username  just for validation , you should not print usernames & passwords in scripts 
+echo "username is $USERNAME" # i am printing the username  just for validation , you should not print usernames & passwords in scripts 
 # here -s means do not echo input coming from terminal
 
 echo "please enter your password:"
 read -s UPASSWORD #the value entered will be automatically atteched to USERNAME variable 
-echon "upassword is $PASSWORD" 
+echo "upassword is $PASSWORD" 
