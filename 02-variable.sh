c2a4b7613d9a776667e952a344d52da8c3d8d60d
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-11 22:28:43 +0530

    pushing second script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-PERSON1= KUMAR 
-PERSON2= YELLAIAH
+PERSON1 = KUMAR 
+PERSON2 = YELLAIAH
 
-echo PERSON1: hi yella
-echo PERSON2: i am fine 
-echo PERSON1: where r u
-echo PERSON2 : pune
+echo $PERSON1: hi yella
+echo $PERSON2: i am fine 
+echo $PERSON1: where r u
+echo $PERSON2 : pune
