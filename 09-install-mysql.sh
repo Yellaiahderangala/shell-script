73f56388a1b0c166366763dee92e29575d912ca0
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-12 10:57:28 +0530

    practice script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -18,3 +18,12 @@ echo "ERROR:: installing mysql is not success"
 else 
 echo "ERROR:: installing mysql is success"
 fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then 
+echo "ERROR:: installing git is not success"
+else 
+echo "ERROR:: installing git is success"
+fi
