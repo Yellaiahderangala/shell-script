7302771d8388d352a4bbe2c21ccfa740367bd5e3
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-12 10:52:55 +0530

    practice script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -11,3 +11,10 @@ echo "you are root user"
 fi # fi means reverse of if , indicating condition end
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+echo "ERROR:: installing mysql is not success"
+else 
+echo "ERROR:: installing mysql is success"
+fi
