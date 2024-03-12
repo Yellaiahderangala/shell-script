44e9333f65e3d97318e1dc0b855bd769665e7539
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-12 10:33:50 +0530

    practice script

---
+++ 09-install-mysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0]
+then 
+echo "ERROR: please run this script with root access"
+exit 1 # you can give other than 0 (beacuse 0 means success so we need to give other than 0 to exit)
+else 
+echo "you are root user"
+fi # fi means reverse of if , indicating condition end
+
