64c2293224c584f93c1a644428e028294d1cbbb9
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-12 08:25:30 +0530

    practice script

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 100 ]
 then
-   echo" the given  $NUMBER is greater than 100"
+   echo "the given number $NUMBER is greater than 100"
 else
-  echo" the given  $NUMBER is not  greater than 100"
+  echo "the given number $NUMBER is not  greater than 100"
 fi
