bdee7f68f00f0ff3cfdef61953ff3fc75be4c744
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-13 10:14:39 +0530

    updating the script

--- 11-logs-update-script.sh
+++ 11-logs-update-script.sh
@@ -14,7 +14,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then echo -e  "ERROR:: $2 .. $R failed $N"
     exit 1
-    else echo "$2 .. $G sucess N"
+    else echo "$2 .. $G sucess $N"
     fi
 }
 
@@ -23,7 +23,7 @@ then
 echo -e   $R "ERROR:: please run this script with root access $N"
 exit 1 # you can give other than 0 (beacuse 0 means success so we need to give other than 0 to exit)
 else 
-echo -e  " $G you are root user $N"
+echo -e  " $G you are root user "
 fi # fi means reverse of if , indicating condition end
 
 yum install mysql -y &>> $LOGFILE
