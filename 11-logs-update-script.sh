473e61c9b03d4c49b0a49a87d9e1ed98e48f1959
yellaiah derangala <yellaiahderangala@gmail.com>
2024-03-13 10:20:21 +0530

    updating the script

--- 11-logs-update-script.sh
+++ 11-logs-update-script.sh
@@ -3,12 +3,11 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
